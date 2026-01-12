37a3ad535b9dbe6c60471885832be746069e0f09
JS-HAN-1987 <58604791+JS-HAN-1987@users.noreply.github.com>
2026-01-12 17:35:35 +0900

    v2.1.3: Add version info to dump_config() for visibility

--- components/esp_sr_doa/esp_sr_doa.cpp
+++ components/esp_sr_doa/esp_sr_doa.cpp
@@ -113,6 +113,8 @@ void ESPSRDOA::loop() {}
 
 void ESPSRDOA::dump_config() {
   ESP_LOGCONFIG(TAG, "ESP-SR DOA:");
+  ESP_LOGCONFIG(TAG, "  Version: v2.1.2");
+  ESP_LOGCONFIG(TAG, "  Build: %s %s", __DATE__, __TIME__);
   LOG_SENSOR("  ", "DOA", this->doa_sensor_);
 }
 
