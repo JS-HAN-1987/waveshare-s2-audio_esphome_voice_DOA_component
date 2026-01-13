9a7752db7e620e74ab6978808acd8eb1477f8879
JS-HAN-1987 <58604791+JS-HAN-1987@users.noreply.github.com>
2026-01-13 21:56:02 +0900

    Fix: Async await syntax error in __init__.py

--- components/esp_sr_doa/__init__.py
+++ components/esp_sr_doa/__init__.py
@@ -19,9 +19,9 @@ CONFIG_SCHEMA = cv.Schema(
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
+    await cg.register_component(var, config)
 
     if "doa" in config:
         sens = await sensor.new_sensor(config["doa"])
